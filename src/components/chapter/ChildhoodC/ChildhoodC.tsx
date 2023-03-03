1d0d8681e21eef89f836ccfed7eb458850c06786
Lucia <lucia.auburger@study.hs-duesseldorf.de>
2023-03-03 21:55:10 +0100

    kurz zusammenfassung Chum Meys leben

--- src/components/chapter/ChildhoodC/ChildhoodC.tsx
+++ src/components/chapter/ChildhoodC/ChildhoodC.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react'
+import { Parallax } from 'react-scroll-parallax'
 import classNames from 'classnames'
 import { Chapter, CHAPTERS } from '../../../helpers/constants'
 import useOnScreen from '../../../helpers/useOnScreenHook'
 import classes from './ChildhoodC.module.scss'
 import sampleGlücklich from '@/assets/img/sample_glücklich.png'
 import sampleArbeit from '@/assets/img/sample_hand.png'
-import sampleTraurig from '@/assets/img/sample_traurig.png'
+import sampleBedruckt from '@/assets/img/sample_bedruckt.png'
+
 
 interface Props {
   setCurrentChapter: (chapter: Chapter) => void
@@ -30,24 +32,38 @@ const ChildhoodC = (props: Props) => {
         </div>
       </div>
       <div className={classes.section}>
-        <img src={sampleGlücklich} alt='Austauschbild' className={classes.img}/>
+        <Parallax speed={5} translateX={['-100px', '-10px']}>
+          <img src={sampleGlücklich} alt='Austauschbild' className={classes.img} />
+        </Parallax>
+        <Parallax speed={5} translateX={['-10px', '-100px']}>
         <p>
           Es war ein Samstag im November 1931, als ein kleiner Junge namens Chum Mey in Thaot Croh geboren wurde.
         </p>
+        </Parallax>
       </div>
       <div className={classes.section}>
-        <img src={sampleArbeit} alt='Austauschbild' className={classes.img}/>
+        <Parallax speed={5} translateX={['-100px', '-10px']}>
+          <img src={sampleArbeit} alt='Austauschbild' className={classes.img} />
+        </Parallax>
+        <Parallax speed={0} translateX={['-10px', '-100px']}>
         <p>
           In diesem Dorf in der Prey Veng Provinz lebte er mit seinen 5 Brüdern und 2 Schwestern und
           arbeitete zusammen mit seinen Eltern auf der Reisfarm. In die Schule ging er allerdings nicht.
         </p>
+        </Parallax>
       </div>
       <div className={classes.section}>
-        <img src={sampleTraurig} alt='Austauschbild' className={classes.img}/>
+        <Parallax speed={5} translateX={['-100px', '-10px']}>
+          <img src={sampleBedruckt} alt='Austauschbild' className={classes.img} />
+        </Parallax>
+        <Parallax speed={5} translateX={['-10px', '-100px']}>
         <p>
-          Als Chum Mey 10 Jahre alt war, starben seine ELtern und er zog mit seinen Geschwistern
-          zu seinem ältesten Bruder nach Ba Phnom.
+          Als Chum Mey 10 Jahre alt war, starben seine Eltern und er zog mit seinen Geschwistern
+            zu seinem ältesten Bruder nach Ba Phnom und arbeitete dort wieder auf einer Farm.
+            Nachdem kurze Zeit später der älteste Bruder starb, brachen schwere Zeiten für die Familie an,
+            da die Ernte nicht genug zum Leben war. INFO FRANZ ZEIT.
         </p>
+        </Parallax>
       </div>
     </div>
   )
--- src/components/chapter/PreInvasionC/PreInvasionC.tsx
+++ src/components/chapter/PreInvasionC/PreInvasionC.tsx
@@ -1,8 +1,13 @@
 import React, { useEffect } from 'react'
+import { Parallax } from 'react-scroll-parallax'
 import classNames from 'classnames'
 import { Chapter, CHAPTERS } from '../../../helpers/constants'
 import useOnScreen from '../../../helpers/useOnScreenHook'
 import classes from './PreInvasionC.module.scss'
+import sampleSorge from '@/assets/img/sample_sorge.png'
+import sampleHappy from '@/assets/img/sample_happy.png'
+import sampleLove from '@/assets/img/sample_love.png'
+import sampleFight from '@/assets/img/sample_fight.png'
 
 interface Props {
   setCurrentChapter: (chapter: Chapter) => void
@@ -26,14 +31,52 @@ const PreInvasionC = (props: Props) => {
           </h2>
         </div>
       </div>
-      <div className='chapter-body-wrapper'>
-        <div className={classes.section}>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, recusandae natus
-            itaque fugit quod optio earum excepturi quae est quibusdam eius deleniti hic, ea
-            consectetur distinctio nobis tempora voluptatum voluptates?
-          </p>
-        </div>
+      <div className={classes.section}>
+        <Parallax speed={5} translateX={['-100px', '-10px']}>
+          <img src={sampleSorge} alt='Austauschbild' className={classes.img} />
+        </Parallax>
+        <Parallax speed={5} translateX={['-10px', '-100px']}>
+        <p>
+            Im Jahr 1950 ging Chum Mey nach Phnom Penh. Dort hatte er große Mühe arbeit zu finden, da er niemanden kannte.
+            Er lebte auf der Straße und fand später als Bootsgehilfe eine Arbeit. Kurz darauf fand er Arbeit als Assistent
+            und lernte dann als Mechaniker.
+            Zu dieser Zeit verdiente er kaum Geld und war immer noch sehr arm.
+        </p>
+        </Parallax>
+      </div>
+      <div className={classes.section}>
+        <Parallax speed={5} translateX={['-100px', '-10px']}>
+          <img src={sampleHappy} alt='Austauschbild' className={classes.img} />
+        </Parallax>
+        <Parallax speed={5} translateX={['-10px', '-100px']}>
+        <p>
+            Vier Jahre später studierte Chum Mey Mechanik in Prey Veng und bekam darauf viel Geld.
+            Zu dieser Zeit reparierte er Fahrzeuge und arbeitete in einer Werkstatt.
+        </p>
+        </Parallax>
+      </div>
+      <div className={classes.section}>
+        <Parallax speed={5} translateX={['-100px', '-10px']}>
+          <img src={sampleLove} alt='Austauschbild' className={classes.img} />
+        </Parallax>
+        <Parallax speed={5} translateX={['-10px', '-100px']}>
+        <p>
+            Im Jahr 1964 arrangierte ein Arbeitskollege für Chum Mey eine Heirat
+            mit dessen Cousine, die er zuvor erst einmal getroffen hatte.
+            Trotzdem führte er eine sehr glückliche Ehe mit ihr und sie bekamen insgesamt 4 Kinder, welche alle im Krieg starben.
+        </p>
+        </Parallax>
+      </div>
+      <div className={classes.section}>
+        <Parallax speed={5} translateX={['-100px', '-10px']}>
+          <img src={sampleFight} alt='Austauschbild' className={classes.img} />
+        </Parallax>
+        <Parallax speed={5} translateX={['-10px', '-100px']}>
+        <p>
+            Kurz vor der Invasion von den Khmer eröffnete Chum Mey kurz hintereinander zwei Werkstätten.
+            Diese verkaufte er am Ende, um dann für einen General im Militär zu arbeiten und für diesen Traktoren und Autos zu reparieren.
+        </p>
+        </Parallax>
       </div>
     </div>
   )
