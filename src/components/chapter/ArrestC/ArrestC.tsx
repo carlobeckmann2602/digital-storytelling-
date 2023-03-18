a698fce66b653b2db224e26a015b71ae5fc15790
Konstantin Haupt <konstantin.haupt@yahoo.de>
2023-03-19 00:20:39 +0100

    feat: Add content for chapter ArrestC

--- src/components/chapter/ArrestC/ArrestC.tsx
+++ src/components/chapter/ArrestC/ArrestC.tsx
@@ -3,6 +3,11 @@ import classNames from 'classnames'
 import { Chapter, CHAPTERS } from '../../../helpers/constants'
 import useOnScreen from '../../../helpers/useOnScreenHook'
 import classes from './ArrestC.module.scss'
+import * as language from './ArrestC_lang'
+import { Parallax } from 'react-scroll-parallax'
+import ArrestImage from '@/assets/img/ArrestC_Painting_Arresting_c_Vann_Nath_khmerrougeincambodia.png'
+import FotographImage from '@/assets/img/ArrestC_Painting_Fotograph_c_Vann_Nath_khmerrougeincambodia.png'
+import Fotograph2Image from '@/assets/img/ArrestC_Painting_Fotograph2_c_Vann_Nath_khmerrougeincambodia.jpg'
 
 interface Props {
   setCurrentChapter: (chapter: Chapter) => void
@@ -27,13 +32,30 @@ const ArrestC = (props: Props) => {
         </div>
       </div>
       <div className='chapter-body-wrapper'>
-        <div className={classes.section}>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, recusandae natus
-            itaque fugit quod optio earum excepturi quae est quibusdam eius deleniti hic, ea
-            consectetur distinctio nobis tempora voluptatum voluptates?
-          </p>
+      <div className={classNames(classes.section, classes.leftAlign)}>
+            <p> {language.T1_Arbeit}</p>
         </div>
+        <div className={classNames(classes.section)}>
+          <Parallax speed={5}>
+            <img src={ArrestImage} className={classNames(classes.imgM)} alt='Testbild' />
+          </Parallax>
+            <p style={{textAlign: 'justify'}}> {language.T2_Verhaftung}</p>
+        </div>
+        <div></div>
+        <div className={classNames(classes.section)}>
+          <div className={classNames(classes.sectionColumn)}>
+            <p>{language.T3_Gefaengnis}</p>
+            <Parallax style={{ paddingTop: '20%' }} speed={5}>
+              <img src={Fotograph2Image} style={{ width: '400px' }} alt='Testbild' />
+            </Parallax>
+          </div>
+          <Parallax speed={5}>
+            <img src={FotographImage} style={{ width: '400px' }} alt='Testbild' />
+          </Parallax>
+        </div>
+        <p className={classNames(classes.highlight)}>
+          {language.T4_Toul}
+        </p>
       </div>
     </div>
   )
