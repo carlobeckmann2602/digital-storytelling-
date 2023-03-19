9bcd490da1ed32b3f58cbbcee24dedc6c60572fe
Lucia <lucia.auburger@study.hs-duesseldorf.de>
2023-03-19 17:40:35 +0100

    new desgin preinvasion bou

--- src/components/chapter/PreInvasionB/PreInvasionB.tsx
+++ src/components/chapter/PreInvasionB/PreInvasionB.tsx
@@ -3,6 +3,15 @@ import classNames from 'classnames'
 import { Chapter, CHAPTERS } from '../../../helpers/constants'
 import useOnScreen from '../../../helpers/useOnScreenHook'
 import classes from './PreInvasionB.module.scss'
+import * as language from './PreInvasionB_lang'
+import { Parallax } from 'react-scroll-parallax'
+import sampleHand from '@/assets/img/sample_hand.png'
+import sampleRevolution from '@/assets/img/sample_revolution.png'
+import sampleLaufen from '@/assets/img/sample_gehen.png'
+import sampleMalen from '@/assets/img/sample_malen.png'
+import sampleZufrieden from '@/assets/img/sample_zufrieden.png'
+import sampleSorge from '@/assets/img/sample_Sorge.png'
+import sampleFight from '@/assets/img/sample_Fight.png'
 
 interface Props {
   setCurrentChapter: (chapter: Chapter) => void
@@ -26,57 +35,47 @@ const PreInvasionB = (props: Props) => {
           </h2>
         </div>
       </div>
-      <div className='chapter-body-wrapper'>
-        <div>
-          <p>
-            Im laufe der Zeit kam Bou Mong immer wieder in Kontakt mit der Revolution. Eines Tages
-            im Jahre 1970 kam ein Kunde ohne Auftrag in sein Geschäft und bat ihn, sich der
-            Revolution anzuschließen. Er lehnte ab, weil er keine Ambitionen hatte, politisch aktiv
-            zu werden.
-          </p>
-        </div>
-        <div>
-          <p>
-            Nach der Arbeit hörte er sich eine Ansprache von Prinz Sihanouk aus Peking an, in der er
-            um die Unterstützung der Revolution zur Befreiung des Landes sowie Herstellung der
-            Unabhängigkeit und des Friedens bittet. Nach dieser Rede entschloss sich Bou Mong, sich
-            der Revolution anzuschließen.
-          </p>
-        </div>
-        <div>
-          <p>
-            Daraufhin machten sich Bou Mong und seine Frau auf den Weg zum Dschungel "Marxist", um
-            sich - wie viele andere Dorfbewohner - der Revolution anzuschließen. Die Reise war
-            kompliziert und führte durch Dschungel und Hügellandschaften.
-          </p>
-        </div>
-        <div>
-          <p>
-            Dort wurden Bou Mong und seine Frau als Maler:in für Propaganda eingesetzt, um Marx,
-            Engels, Lenin und die Revolutionsflaggen mit Hammer und Sichel zu malen.
-          </p>
-        </div>
-        <div>
-          <p>
-            Während seiner Zeit dort, sollte Bou Mong eine Biografie schreiben. Als Autor änderte er
-            seinen Namen von Bou Mong-Seang zu Bou Meng, um seine Identität vor Angriffen außerhalb
-            und innerhalb der Revolutionsbewegung zu schützen.
-          </p>
-        </div>
-        <div>
-          <p>
-            Die Revolutionsbewegung wurde im Dschungel regelmäßig von den Truppen von Lol Non
-            bombadiert und verlegte deshalb regelmäßig den Standort.
-          </p>
-        </div>
-        <div>
-          <p>
-            Bou Meng wurde eine Beschäftigung in Druckerei zugeteilt. Er und seine Kameraden
-            erhielten täglich Meldungen von der Front und erfuhren von der Umstellung Phom Penhs und
-            dem erfolgreichen Angriff auf die Verteidigung von Lon Nols Truppen. Er freute sich über
-            den bevorstehenden Sieg.
-          </p>
-        </div>
+      <div className={classes.section}>
+        <Parallax speed={10}>
+          <img src={sampleHand} alt='Austauschbild' className={classes.img} />
+        </Parallax>
+        <p>{language.b_erste_kontakte}</p>
+      </div>
+      <div className={classes.section}>
+        <p>{language.b_ansprache}</p>
+        <Parallax speed={10}>
+          <img src={sampleRevolution} alt='Austauschbild' className={classes.img} />
+        </Parallax>
+      </div>
+      <div className={classes.section}>
+        <Parallax speed={10}>
+          <img src={sampleLaufen} alt='Austauschbild' className={classes.img} />
+        </Parallax>
+        <p>{language.b_aufbruch}</p>
+      </div>
+      <div className={classes.section}>
+        <p>{language.b_maler}</p>
+        <Parallax speed={10}>
+          <img src={sampleMalen} alt='Austauschbild' className={classes.img} />
+        </Parallax>
+      </div>
+      <div className={classes.section}>
+        <Parallax speed={10}>
+          <img src={sampleZufrieden} alt='Austauschbild' className={classes.img} />
+        </Parallax>
+        <p>{language.b_biographie}</p>
+      </div>
+      <div className={classes.section}>
+        <p>{language.b_bomabadiert}</p>
+        <Parallax speed={10}>
+          <img src={sampleSorge} alt='Austauschbild' className={classes.img} />
+        </Parallax>
+      </div>
+      <div className={classes.section}>
+        <Parallax speed={10}>
+          <img src={sampleFight} alt='Austauschbild' className={classes.img} />
+        </Parallax>
+        <p>{language.b_druckerei}</p>
       </div>
     </div>
   )
--- src/components/chapter/PreInvasionB/PreInvasionB_lang.ts
+++ src/components/chapter/PreInvasionB/PreInvasionB_lang.ts
@@ -1,1 +1,21 @@
-export const HEADLINE = 'Beispiel Headline' // TODO: Replace with real language keys
+import exp from 'constants'
+
+export const b_erste_kontakte =
+  'Im laufe der Zeit kam Bou Mong immer wieder in Kontakt mit der Revolution. Eines Tages im Jahre 1970 kam ein Kunde ohne Auftrag in sein Geschäft und bat ihn, sich der Revolution anzuschließen. Er lehnte ab, weil er keine Ambitionen hatte, politisch aktiv zu werden.'
+export const b_ansprache =
+  'Nach der Arbeit hörte er sich eine Ansprache von Prinz Sihanouk aus Peking an, in der er um die Unterstützung der Revolution zur Befreiung des Landes sowie Herstellung der Unabhängigkeit und des Friedens bittet. Nach dieser Rede entschloss sich Bou Mong, sich der Revolution anzuschließen.'
+
+export const b_aufbruch =
+  'Daraufhin machten sich Bou Mong und seine Frau auf den Weg zum Dschungel "Marxist", um sich - wie viele andere Dorfbewohner - der Revolution anzuschließen. Die Reise war kompliziert und führte durch Dschungel und Hügellandschaften.'
+
+export const b_maler =
+  'Dort wurden Bou Mong und seine Frau als Maler:in für Propaganda eingesetzt, um Marx, Engels, Lenin und die Revolutionsflaggen mit Hammer und Sichel zu malen.'
+
+export const b_biographie =
+  'Während seiner Zeit dort, sollte Bou Mong eine Biografie schreiben. Als Autor änderte er seinen Namen von Bou Mong-Seang zu Bou Meng, um seine Identität vor Angriffen außerhalb und innerhalb der Revolutionsbewegung zu schützen.'
+
+export const b_bomabadiert =
+  'Die Revolutionsbewegung wurde im Dschungel regelmäßig von den Truppen von Lol Non bombadiert und verlegte deshalb regelmäßig den Standort.'
+
+export const b_druckerei =
+  'Bou Meng wurde eine Beschäftigung in Druckerei zugeteilt. Er und seine Kameraden erhielten täglich Meldungen von der Front und erfuhren von der Umstellung Phom Penhs und dem erfolgreichen Angriff auf die Verteidigung von Lon Nols Truppen. Er freute sich über den bevorstehenden Sieg.'
