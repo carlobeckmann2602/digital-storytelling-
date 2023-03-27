28eb02074d3f8bc3068d1971015d53e93748a44a
Lucia <lucia.auburger@study.hs-duesseldorf.de>
2023-03-27 16:14:14 +0200

    made the layout sample text and sample image

--- src/components/chapter/InvasionBackground/InvasionBackground.tsx
+++ src/components/chapter/InvasionBackground/InvasionBackground.tsx
@@ -3,6 +3,9 @@ import classNames from 'classnames'
 import { Chapter, CHAPTERS } from '../../../helpers/constants'
 import useOnScreen from '../../../helpers/useOnScreenHook'
 import classes from './InvasionBackground.module.scss'
+import * as language from './InvasionBackground_lang'
+import { Parallax } from 'react-scroll-parallax'
+import TestImage from '@/assets/img/template-1.jpg'
 
 interface Props {
   setCurrentChapter: (chapter: Chapter) => void
@@ -32,15 +35,19 @@ const InvasionBackground = (props: Props) => {
         </div>
       </div>
       <div className='chapter-body-wrapper'>
-        <div className={classes.section}>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, recusandae natus
-            itaque fugit quod optio earum excepturi quae est quibusdam eius deleniti hic, ea
-            consectetur distinctio nobis tempora voluptatum voluptates?
-          </p>
+        <div className={classNames(classes.section)}>
+          <Parallax speed={10}>
+            <img src={TestImage} style={{ width: '400px' }} alt='Testbild' />
+          </Parallax>
+          <p style={{ alignSelf: 'center', textAlign: 'justify' }}>{language.teil1}</p>
+        </div>
+        <div className={classNames(classes.section)}>
+          <p style={{ alignSelf: 'center', textAlign: 'justify' }}>{language.teil2}</p>
+          <Parallax speed={10}>
+            <img src={TestImage} style={{ width: '400px' }} alt='Testbild' />
+          </Parallax>
         </div>
       </div>
-      <div ref={bottomRef}></div>
     </div>
   )
 }
