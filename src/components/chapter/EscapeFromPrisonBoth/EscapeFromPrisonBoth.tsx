339a05cc347ed8c3ec34716387638654e2704d3b
Carlo Beckmann <business@carlo-beckmann.com>
2023-03-28 21:03:18 +0200

    feat: create text elemente

--- src/components/chapter/EscapeFromPrisonBoth/EscapeFromPrisonBoth.tsx
+++ src/components/chapter/EscapeFromPrisonBoth/EscapeFromPrisonBoth.tsx
@@ -3,6 +3,11 @@ import classNames from 'classnames'
 import { Chapter, CHAPTERS } from '../../../helpers/constants'
 import useOnScreen from '../../../helpers/useOnScreenHook'
 import classes from './EscapeFromPrisonBoth.module.scss'
+import * as language from './EscapeFromPrisonBoth_lang'
+import { Parallax } from 'react-scroll-parallax'
+
+import EscapeFromPrisopnImage1 from '@/assets/img/InvasionBoth_Invasion1_c_Roland_Neveu.png'
+import Placeholder from '@/assets/img/Placeholder.png'
 
 interface Props {
   setCurrentChapter: (chapter: Chapter) => void
@@ -22,6 +27,17 @@ const EscapeFromPrisonBoth = (props: Props) => {
     bottomOnScreen && props.setCurrentChapter(CHAPTER_ID)
   }, [bottomOnScreen])
 
+  /* Formula (all elements must have the same idKey):
+  (Height of own sticky-container)
+  + (Height of Content of other sides container)
+  + (Height of next sticky-container of other side) */
+  const computeHeight = (idKey: string, offset = 0) => {
+    const elements = document.querySelectorAll(idKey)
+    let height = offset
+    elements.forEach((elem) => (height += elem.getBoundingClientRect().height))
+    return height
+  }
+
   return (
     <div id={CHAPTER_ID}>
       <div ref={topRef} className={'header-outer'}>
@@ -33,11 +49,149 @@ const EscapeFromPrisonBoth = (props: Props) => {
       </div>
       <div className='chapter-body-wrapper'>
         <div className={classes.section}>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, recusandae natus
-            itaque fugit quod optio earum excepturi quae est quibusdam eius deleniti hic, ea
-            consectetur distinctio nobis tempora voluptatum voluptates?
-          </p>
+          <div className={classes.splitScreenWrapper}>
+            <div className={classNames(classes.leftSide)}>
+              {/* 1.section LEFT */}
+              <div className={classNames(classes.section, 'firstR')}>
+                <div className={classNames(classes.subsection, classes.leftAlign)}>
+                  <p>{language.C_1}</p>
+                </div>
+                <div className={classNames(classes.subsection, classes.rightAlign)}>
+                  <Parallax speed={3}>
+                    <img src={Placeholder} alt='Testbild' className={classNames(classes.imgS)} />
+                  </Parallax>
+                </div>
+                <div className={classNames(classes.subsection, classes.rightAlign)}>
+                  <p>{language.C_1}</p>
+                </div>
+                <div className={classNames(classes.subsection, classes.rightAlign)}>
+                  <Parallax speed={3}>
+                    <img src={Placeholder} alt='Testbild' className={classNames(classes.imgS)} />
+                  </Parallax>
+                </div>
+                <div className={classNames(classes.subsection, classes.leftAlign)}>
+                  <p>{language.C_1}</p>
+                </div>
+              </div>
+              {/* 2.section LEFT */}
+              <div style={{ height: computeHeight('.secondL', 450) }}>
+                <div className={classNames(classes.section, classes.sticky, 'firstR', 'secondL')}>
+                  <div className={classNames(classes.subsection)}>
+                    <p className={classNames(classes.highlight)}>{language.C_1}</p>
+                  </div>
+                </div>
+              </div>
+              {/* 3.section LEFT */}
+              <div className={classNames(classes.section, 'thirdR')}>
+                <div className={classNames(classes.subsection, classes.rightAlign)}>
+                  <p>{language.C_1}</p>
+                </div>
+                <div className={classNames(classes.subsection, classes.leftAlign)}>
+                  <p>{language.C_1}</p>
+                </div>
+              </div>
+              {/* 4.section LEFT */}
+              <div style={{ height: computeHeight('.fourthL', 450) }}>
+                <div className={classNames(classes.section, classes.sticky, 'thirdR', 'fourthL')}>
+                  <div className={classNames(classes.subsection, classes.rightAlign)}>
+                    {/* <Parallax speed={3}> */}
+                    <img src={Placeholder} alt='Testbild' className={classNames(classes.imgS)} />
+                    {/* </Parallax> */}
+                  </div>
+                  <div className={classNames(classes.subsection, classes.leftAlign)}>
+                    <p>{language.C_1}</p>
+                  </div>
+                </div>
+              </div>
+              {/* 5.section LEFT */}
+              <div className={classNames(classes.section, 'fifthR')}>
+                <div className={classNames(classes.subsection, classes.rightAlign)}>
+                  {/* <Parallax speed={3}> */}
+                  <img src={Placeholder} alt='Testbild' className={classNames(classes.imgL)} />
+                  {/* </Parallax> */}
+                </div>
+                <div className={classNames(classes.subsection, classes.leftAlign)}>
+                  <p>{language.C_1}</p>
+                </div>
+                <div className={classNames(classes.spacerM)}></div>
+                <div className={classNames(classes.subsection, classes.center)}>
+                  <Parallax speed={-5}>
+                    <p className={classNames(classes.highlight)}>{language.C_1}</p>
+                  </Parallax>
+                </div>
+                <div className={classNames(classes.spacerL)}></div>
+                <div className={classNames(classes.subsection, classes.center)}>
+                  <Parallax speed={-5}>
+                    <p className={classNames(classes.highlight)}>{language.C_1}</p>
+                  </Parallax>
+                </div>
+                <div className={classNames(classes.spacerL)}></div>
+                <div className={classNames(classes.subsection, classes.center)}>
+                  <Parallax speed={-5}>
+                    <p className={classNames(classes.highlight)}>{language.C_1}</p>
+                  </Parallax>
+                </div>
+                <div className={classNames(classes.spacerM)}></div>
+                <div className={classNames(classes.subsection)}>
+                  <p>{language.C_1}</p>
+                </div>
+              </div>
+            </div>
+            <hr className={classes.dashed}></hr>
+            <div className={classes.rightSide}>
+              {/* Offset container for first section */}
+              <div style={{ height: computeHeight('.firstR', 300) }}></div>
+              {/* 1.section RIGHT */}
+              <div className={classNames(classes.section, 'secondL')}>
+                <div className={classNames(classes.subsection, classes.leftAlign)}>
+                  <p>{language.B_1}</p>
+                </div>
+                <div className={classNames(classes.subsection, classes.rightAlign)}>
+                  <p>{language.B_1}</p>
+                </div>
+                <div className={classNames(classes.subsection, classes.rightAlign)}>
+                  <Parallax speed={2}>
+                    <img src={Placeholder} alt='Testbild' className={classNames(classes.imgS)} />
+                  </Parallax>
+                </div>
+              </div>
+              {/* 2.section RIGHT */}
+              <div style={{ height: computeHeight('.thirdR', 450) }}>
+                <div className={classNames(classes.section, classes.sticky, 'secondL', 'thirdR')}>
+                  <div className={classNames(classes.subsection, classes.leftAlign)}>
+                    <p>{language.B_1}</p>
+                  </div>
+                  <div className={classNames(classes.subsection, classes.rightAlign)}>
+                    {/* <Parallax speed={2}> */}
+                    <img src={Placeholder} alt='Testbild' className={classNames(classes.imgS)} />
+                    {/* </Parallax> */}
+                  </div>
+                </div>
+              </div>
+              {/* 3.section RIGHT */}
+              <div className={classNames(classes.section, 'fourthL')}>
+                <div className={classNames(classes.subsection, classes.leftAlign)}>
+                  <p>{language.B_1}</p>
+                </div>
+                <div className={classNames(classes.subsection, classes.rightAlign)}>
+                  <p>{language.B_1}</p>
+                </div>
+                <div className={classNames(classes.subsection, classes.rightAlign)}>
+                  <Parallax speed={2}>
+                    <img src={Placeholder} alt='Testbild' className={classNames(classes.imgS)} />
+                  </Parallax>
+                </div>
+              </div>
+              {/* 4.section RIGHT */}
+              <div style={{ height: computeHeight('.fifthR', 150) }}>
+                <div className={classNames(classes.section, classes.sticky, 'fourthL', 'fifthR')}>
+                  <div className={classNames(classes.subsection)}>
+                    <p>{language.B_1}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div ref={bottomRef}></div>
--- src/components/chapter/EscapeFromPrisonBoth/EscapeFromPrisonBoth_lang.ts
+++ src/components/chapter/EscapeFromPrisonBoth/EscapeFromPrisonBoth_lang.ts
@@ -1,1 +1,45 @@
 export const HEADLINE = 'Beispiel Headline' // TODO: Replace with real language keys
+
+/* ------------------------------------------------------------------------------ */
+/*                              Chum Mey                                          */
+/* ------------------------------------------------------------------------------ */
+
+export const C_1 =
+  'Insassen flüchten zusammen mit Gefängniswärtern, die Situation ist unübersichtlich. Die Gefängniswärter  brachten die Gefangenen ohne Fesseln oder Augenbinden aus dem Gefängnis. Sie töteten diejenigen, die noch an ihre Betten gefesselt waren. Danach führten sie alle Gefangenen nach Choeung Ek. Die Gefangenen dachten, dass sie dort getötet werden würden, aber bei einem Gespräch der Wärter bekamen sie mit, dass sie das nicht tun wollten, da das zu lange dauern würde. Sie wurden schließlich zu anderen Gefangenen nach Prey Sar geführt, wo auch seine Frau mit ihrem Baby war. Er durfte zu ihr, sprach aber aus Angst vor Schlägen nicht mir ihr.'
+
+export const C_2 =
+  'Vietnamesen kommen auf Autos und eröffnen das Feuer,  die Gefangenen versuchen zu fliehen. U.a. Bou Meng entkommt, Chum Mey bleibt aber bei seiner Frau, die aufgrund ihrer kürzlichen Geburt zu geschwächt zum Fliehen ist.'
+
+export const C_3 =
+  'Sie (er, Frau + Kind + Eng) treffen in Srah Thol ein. Chum Mey fragt bewaffnete rote Khmer nach Essen. Sie sind skeptisch, lenken aber ein und sagen, sie sollen auf einen Passierschein von Angkar warten. Sie (Chum Mey, Frau und Kind + Eng) teilen sich daraufhin alle ein gemeinsames Bett.'
+
+export const C_4 =
+  'Chum Mey bemerkt, dass wiederkehrende Soldaten fragen, wo die Personen sind, die sie töten sollen. Gegenüber Chum Mey behaupten sie, sie hätten die Passierscheine, und führten sie aus dem Dorf. Dann finden sie an zu schießen. Chum Mey entkam, hörte aber noch das Schreien seines Babys; Alle außer ihm werden erschossen.'
+
+export const C_5 =
+  'Er wanderte auf seiner Flucht 6 Tage und trank kein Wasser. Er musste dabei ständig an seine Frau und Kind denken. Er war sich jedoch bewusst, dass er auch erschossen werden würde, wenn er zurückkehrte. Er wollte zurück nach Phnom Penh kommen, und traf auf dem Weg in Krel u.a. Sam Thoeun (seine zukünftige Frau), die früher mit seiner früheren Frau in der O-Russei Näherei arbeitete.'
+
+export const C_6 =
+  'Chum Mey  fand in einer Fabrik Arbeit. Nach einem Überfall von vietnamesischen Truppen  heiratete er Sam Thoeun in einer kleinen Zeremonie (nur Opfergaben, er selbst musste sich Kleidung leihen, da er nur seine zerissene Kleidung aus Toul Sleng hatte). Sie machten sich nach Phom Penh auf, um Arbeit und ein Zuhause zu finden. Die neue Regierung suchte nach Mechanikern mit Erfahrung, Chum Mey meldete sich auf die Stelle.'
+
+/* ------------------------------------------------------------------------------ */
+/*                              Bou Meng                                          */
+/* ------------------------------------------------------------------------------ */
+
+export const B_1 =
+  'Bou Meng arbeitet, wie an jedem anderen Tag im Gefängnis. Bis um 9:00 Uhr morgens plötzlich alle aufgefordert werden ihre Arbeit niederzulegen und gefesselt werden. Alle Insassen werden dann von vier Wärtern in einen geschlossenenden Raum im Gefängnis festgehalten. Die Wäter warten dort über 5 Stunden auf weiter Anweisungen. Während dessen sind in der ferne immer mehr Schüsse zuhören. Bou Meng hat das Gefühl, dass sein Todestag heute gekommen ist.'
+
+export const B_2 =
+  'Um ca 18:00 Uhr Abends ging es dann, diesmal ungefesselt, für alle Gefangenden mit den Wärtern weiter aus dem Gefängnis raus. Die Wärter fürhren die circa 10 Gefangenden erst Richtung Choeung Ek weiter. Dieser Ort ist dabei ein Teil des Gefängnisses S-21 in dem viele Insassen während der Zeit des Gefängnisses hingerichtet wurden. Bou Meng bemerkt dies auf der Flucht, aber nicht da es dunkel ist einzig allein der Geruch der vielen Leichen nimmt er wahr, diesen ordnet er aber toten Tieren zu. Dieser Ort ist aber nicht der finale Punkt der Flucht. Sie laufen weiter bis sie um 5:00 Uhr morgens final in einem Dorf in der Provinz Kampong Speu ankommen.'
+
+export const B_3 =
+  'In dem Dorf treffen Bou Meng und die anderen Mitinsassen, sowie die vier Wärter aus dem S-21 auf vietnamesische Truppen. Aus Angst fliehen die Wärter und überlassen die Gefangen sich selber. Alle Gefangen an in jegliche Richtung zu fliehen. Bou Meng flieht nur mit seinem Freund Phan Thron-Chan in nordliche Richtung. Auf der Flucht finden die beiden wenig Essen und zu trinken, weshalb sie immer wieder wilde Früchte oder gar Wurzeln von Pflanzen essen müssen. Um in der Nacht etwas schlaf zufinden haben sie sich Klamotten von Toten genommen und gewaschen.'
+
+export const B_4 =
+  'Auf ihrer Flucht müssen sie dauerhaft in Bewegung bleiben, da sie immer wieder mit dem Krieg in Kontakt kommen und Angst haben müssen zwischen die Fronten zugeraten. Die beiden befinden sich dabei immer in einem Gebiet, welches von den Roten Khmer kontrolliert wird. Aus diesem Grund werden sie nach einigen Tagen auch von diesen immer weiter in die Berge getrieben, wo die Roten Khmer vor haben alle neu zugruppieren. Dabei heißt die Devise für alle nicht auf zu fallen und auf keinen Fall vor den roten Khmer zufliehen, da jeder abtrünige direkt erschossen wird.'
+
+export const B_5 =
+  'Die Verhältnisse unter den Roten Khmer verbessert sich dabei für die beiden nicht. Sie verschlechter sich eher noch, da jeder gerade genug Kraft hat um sich um sich slebst zu kümmern. Des Weiteren müssen die beiden sehr vorsichtig mit ihrer Vergangenheit als S-21 Insassen umgehen, weshalb sie sich entscheiden dies zu verheimlichen. Sie kommen bei ihrer Flucht häufiger in Orten an an denen sie arbeiten könnten, ziehen aber immer weiter, da sie ANgst haben, dass ihr Hintergrund irgendwann heraus kommen könnte, wen sie zu lange an einem Ort sind.'
+
+export const B_6 =
+  'Ihre Flucht endet in Mok Rea, wo Bou Meng sich aber sehr unwohl fühlt, da er entdeckt, dass dort viele Kamobschianer von den roten Khmer dort hingerichtet worden sind. Im späten Feburar 1979 werden die beide dann final aus den Fengen der roten Khmer von vietnamesichen Truppen befreit, da Bou Mengs Freund Chan der vietnamesischen Sprache mächtig war und die eintreffenden Truppen so überzeugen konnte. Die beiden wurden dann von den Vietnamesen weggebracht und haben von diesen neue Jobs bekommen. Bou Meng arbeitet ab da an im provinzialen Verwaltungsamt und muss dort erneut Bilder von unteranderem Marx und Lenin malen.'
