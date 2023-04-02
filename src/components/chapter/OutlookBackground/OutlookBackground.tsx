0b590cb99fdbd8d3d0f35107bc655e3f005fa444
Konstantin Haupt <konstantin.haupt@yahoo.de>
2023-04-02 18:06:59 +0200

    feat: Add content for chapter OutlookBackground

--- src/components/chapter/OutlookBackground/OutlookBackground.tsx
+++ src/components/chapter/OutlookBackground/OutlookBackground.tsx
@@ -3,6 +3,17 @@ import classNames from 'classnames'
 import { Chapter, CHAPTERS } from '../../../helpers/constants'
 import useOnScreen from '../../../helpers/useOnScreenHook'
 import classes from './OutlookBackground.module.scss'
+import * as language from './OutlookBackground_lang'
+import { Parallax } from 'react-scroll-parallax'
+import Quotation from '../../Quotation/Quotation'
+import TrialImage from '@/assets/img/OutlookBackground_trial_c_bbc_gettyImages.png'
+import Trial2Image from '@/assets/img/OutlookBackground_Trial2_c_AFP_Photo_ECCC_Mark_Peters.jpg'
+import MuseumImage from '@/assets/img/OutlookBackground_museum_c_bbc_gettyImages.png'
+import { ReactComponent as ArrowIcon } from '@/assets/img/icons/arrow-up-icon.svg'
+import Painting1Image from '@/assets/img/OutlookBackground_Painting_Arresting_c_Vann_Nath_khmerrougeincambodia.png'
+import Painting2Image from '@/assets/img/OutlookBackground_Painting_Blindfolded_c_Bou_Meng_2004_DC-Cam_Archives.png'
+import Painting3Image from '@/assets/img/OutlookBackground_Painting_Prisoners_c_Vann_Nath_khmerrougeincambodia.jpg'
+import { motion } from 'framer-motion'
 
 interface Props {
   setCurrentChapter: (chapter: Chapter) => void
@@ -22,6 +33,14 @@ const OutlookBackground = (props: Props) => {
     bottomOnScreen && props.setCurrentChapter(CHAPTER_ID)
   }, [bottomOnScreen])
 
+  const scrollToTop = () => {
+    console.log('test')
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+
   return (
     <div id={CHAPTER_ID}>
       <div ref={topRef} className={'header-outer'}>
@@ -32,13 +51,76 @@ const OutlookBackground = (props: Props) => {
         </div>
       </div>
       <div className='chapter-body-wrapper'>
-        <div className={classes.section}>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, recusandae natus
-            itaque fugit quod optio earum excepturi quae est quibusdam eius deleniti hic, ea
-            consectetur distinctio nobis tempora voluptatum voluptates?
-          </p>
+        <div className={classNames(classes.sectionColumn2)}>
+          <p className={classNames(classes.headerText)}>{language.H1_Museum}</p>
+          <p> {language.T1_Museum}</p>
         </div>
+        <Parallax speed={5}>
+          <img
+            src={MuseumImage}
+            style={{ width: '80%' }}
+            className={classNames(classes.imgM)}
+            alt='Testbild'
+          />
+        </Parallax>
+        <div className={classNames(classes.section)}>
+          <div className={classNames(classes.sectionColumn2)}>
+            <p className={classNames(classes.headerText)}>{language.H2_Besuch}</p>
+            <p className={classNames(classes.text)}> {language.T2_Besuch}</p>
+          </div>
+          <div className={classNames(classes.sectionColumnImg)}>
+            <Parallax style={{ alignSelf: 'flex-end' }} speed={5}>
+              <img src={Painting1Image} alt='Testbild' />
+            </Parallax>
+            <Parallax style={{ alignSelf: 'flex-start' }} speed={9}>
+              <img src={Painting2Image} alt='Testbild' />
+            </Parallax>
+            <Parallax style={{ alignSelf: 'flex-end' }} speed={7}>
+              <img src={Painting3Image} alt='Testbild' />
+            </Parallax>
+          </div>
+        </div>
+        <div className={classNames(classes.section)}>
+          <Parallax speed={5}>
+            <img src={Trial2Image} style={{ width: '500px' }} alt='Testbild' />
+          </Parallax>
+          <div className={classNames(classes.sectionColumn2)}>
+            <p className={classNames(classes.headerText)}>{language.H3_Aufarbeitung}</p>
+            <p className={classNames(classes.text)}>{language.T3_Aufarbeitung}</p>
+          </div>
+        </div>
+        <div className={classNames(classes.section)}>
+          <div style={{ paddingTop: '200px' }} className={classNames(classes.sectionColumn2)}>
+            <p className={classNames(classes.headerText)}>{language.H4_Verhandlung}</p>
+            <p className={classNames(classes.text)}> {language.T4_Verhandlung}</p>
+          </div>
+          <Parallax speed={5}>
+            <img src={TrialImage} className={classNames(classes.imgM)} alt='Testbild' />
+          </Parallax>
+        </div>
+        <div className={classNames(classes.spacer)}></div>
+        <div className={classNames(classes.quoteContainer)}>
+          <div>
+            <Quotation
+              quote={language.Q1_Gericht}
+              citation={'Chum Mey'}
+              colorCode={'chum'}
+            ></Quotation>
+          </div>
+        </div>
+        <div className={classNames(classes.spacer)}></div>
+        <Quotation quote={language.Q2_Nacht} citation={'Chum Mey'} colorCode={'chum'}></Quotation>
+        <div style={{ height: '100px' }}></div>
+        <motion.div style={{ scale: 0 }} animate={{ scale: 1 }} className={classes.buttonWrapper}>
+          <motion.button
+            animate={{ scale: 1.4, translateX: '3px' }}
+            className={classes.backButton}
+            onClick={() => scrollToTop()}
+          >
+            <ArrowIcon className={classes.arrowIcon} />
+            <p>Zurück zum Anfang</p>
+          </motion.button>
+        </motion.div>
       </div>
       <div ref={bottomRef}></div>
     </div>
