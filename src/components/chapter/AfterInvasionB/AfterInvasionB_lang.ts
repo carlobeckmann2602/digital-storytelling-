4fd8fd4bde9954f028ada619e7493ef106a38174
Janne Wernecken <janne.wernecken@study.hs-duesseldorf.de>
2023-04-28 13:34:01 +0200

    fix: Typos

--- src/components/chapter/AfterInvasionB/AfterInvasionB_lang.ts
+++ src/components/chapter/AfterInvasionB/AfterInvasionB_lang.ts
@@ -5,7 +5,8 @@ export const T1_Ankunft =
 export const T2_Kinder =
   'Seine Kinder dürfen dabei nicht bei ihnen bleiben, sondern kommen in ein Kinderheim. Von anderen Familien in der Umgebung hört er, dass auch deren Kinder von ihren Eltern getrennt werden. Außerdem bekommen sie alle einheitliche Kleidung, die von der Organisation Angka ausgegeben wird. Die Roten Khmer sprechen oft von Angka, doch trotzdem können Bou Meng und seine Kollegen  nicht genau sagen, was diese Organisation mit ihnen zu tun hat und was sie macht. Sie sind sich nur einig, dass es besser ist, ihr gegenüber nicht negativ aufzufallen.'
 
-export const I1_Angka = 'Die Roten Khmer nutzten die Scheinorganisation Angka padevat (kurz Angka, deutsch: revolutionäre Organisation), um ihre Handlungen geheim zu halten und zu rechtfertigen. Den Kambodschanern wurde die Zugehörigkeit von Angka zu der KPK bzw. den Roten Khmer lange verschwiegen und stattdessen als allwissende Macht vermittelt. Hierdurch sollten die Kambodschaner dazu angehalten werden, gehorsam zu sein und keine kritischen Fragen zu stellen.'
+export const I1_Angka =
+  'Die Roten Khmer nutzten die Scheinorganisation Angka padevat (kurz Angka, deutsch: revolutionäre Organisation), um ihre Handlungen geheim zu halten und zu rechtfertigen. Den Kambodschanern wurde die Zugehörigkeit von Angka zu der KPK bzw. den Roten Khmer lange verschwiegen und stattdessen als allwissende Macht vermittelt. Hierdurch sollten die Kambodschaner dazu angehalten werden, gehorsam zu sein und keine kritischen Fragen zu stellen.'
 
 export const T3_Bio =
   'Bou Meng muss immer wieder seine Biographie aufschreiben und darin die Namen seiner Verwandten sowie seine Rolle in der Revolution angeben. Deshalb bekommt er langsam das Gefühl, dass die Roten Khmer ihm seine Herkunftsgeschichte nicht glauben. Doch da sie ihm keine Rückmeldung geben, hält er es für das Beste, sich nicht zu viele Gedanken darüber zu machen.'
@@ -23,4 +24,4 @@ export const T6_Verschwinden =
   'Auch wenn ihm ein paar Gesichter mit der Zeit immer vertrauter werden, sind die Bekanntschaften nie von langer Dauer. An einem Tag teilt er mit jemandem noch sein Essen, am nächsten Tag erscheint dieser nicht mehr zur Arbeit. Was mit ihnen passiert ist, weiß niemand. Anstelle der verschwundenen Insassen tauchen täglich neue Gesichter auf, die Bou Meng zuvor noch nie gesehen hat.'
 
 export const T7_Angst =
-  'So vergehen Tage, Wochen, Monate. Unter den unmenschlichen Bedingungen schläft er jeden Abend mit der Angst ein, am nächsten Morgen selbst eine der verschwundenen Personen zu sein. Gleichzeitig fragt er sich immer wieder, was er falsch gemacht hatte, um so ins Visir der roten Khmer zu geraten...'
+  'So vergehen Tage, Wochen, Monate. Unter den unmenschlichen Bedingungen schläft er jeden Abend mit der Angst ein, am nächsten Morgen selbst eine der verschwundenen Personen zu sein. Gleichzeitig fragt er sich immer wieder, was er falsch gemacht hatte, um so ins Visier der roten Khmer zu geraten...'
